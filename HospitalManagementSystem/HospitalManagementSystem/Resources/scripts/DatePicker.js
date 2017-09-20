9f3a60cf315c3b578264c3cccef518b492fe84fc
Anjali Nirmani <anjali.nirmani@ifsworld.com>
2017-09-20 11:20:01 +0530

    Added App_Code & Resources folders
    added images, css, scripts

---
+++ HospitalManagementSystem/HospitalManagementSystem/Resources/scripts/DatePicker.js
@@ -0,0 +1,11 @@
+﻿
+    $(document).ready(function () {
+        $("#txtOrDate").datepicker({ minDate: 0, numberOfMonths: 1, onSelect: function (selected) { $("#txtReDate").datepicker("option", "minDate", selected) } });
+        $("#txtReDate").datepicker({ minDate: 0, numberOfMonths: 1, onSelect: function (selected) { $("#txtOrDate").datepicker("option", "maxDate", selected) } });
+        $("#txtOrDate").prop("readonly", true);
+        $("#txtReDate").prop("readonly", true);
+
+        $("#submit").click(function () {
+
+        });
+    });
