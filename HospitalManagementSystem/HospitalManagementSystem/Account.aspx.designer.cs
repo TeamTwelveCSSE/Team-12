597373e6eca220c638e55305f42c80e7d358bb3a
thamalisathsara <thamalish@gmail.com>
2017-10-03 13:58:52 +0530

    Thamali - Expenses.aspx

--- HospitalManagementSystem/HospitalManagementSystem/Account.aspx.designer.cs
+++ HospitalManagementSystem/HospitalManagementSystem/Account.aspx.designer.cs
@@ -101,5 +101,23 @@ namespace HospitalManagementSystem {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCancel;
+        
+        /// <summary>
+        /// gridview1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridview1;
+        
+        /// <summary>
+        /// datasource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource datasource1;
     }
 }
